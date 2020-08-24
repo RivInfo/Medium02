8d3cd231e77cffb3fc9fa00cb5e3f17dc949b25e
roman <roman@pushkeen.ru>
2020-08-24 23:13:12 +0300

    Правки3

--- medium8-1-3.cs
+++ medium8-1-3.cs
@@ -1,34 +1,51 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 class Bag
 {
-    public readonly List<Item> _items;
-    private readonly int _maxWeidth; 
+    private List<Item> _items;
+    private readonly int _maxWeidth;
 
     public Bag(int maxWeidth, List<Item> items)
     {
         _maxWeidth = maxWeidth;
         _items = items;
-    } 
+    }
 
     public void AddItem(string name, int count)
     {
-        int currentWeidth = Items.Sum(item => item.Count);
-        Item targetItem = Items.FirstOrDefault(item => item.Name == name);
+        int currentWeidth = _items.Sum(item => item.Count);
+        Item targetItem = _items.FirstOrDefault(item => item.Name == name);
 
         if (targetItem == null)
             throw new InvalidOperationException();
 
-        if (currentWeidth + count > MaxWeidth)
+        if (currentWeidth + count > _maxWeidth)
             throw new InvalidOperationException();
 
-        targetItem.Count += count;
+        targetItem.CountPlus(count);
+    }
+
+    public IEnumerable<Item> GetItems()
+    {
+        return _items.AsEnumerable();
     }
 }
 
 class Item
 {
-    public int Count;
     public readonly string Name;
+    public int Count { get; private set; }
 
+    public Item(string name, int count)
+    {
+        Name = name;
+        Count = count;
+    }
+
+    public void CountPlus(int count)
+    {
+        Count += count;
+    }
 }
